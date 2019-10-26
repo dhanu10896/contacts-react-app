a1cc3498938d95c119fb0643a069637e20e538fb
Dhananjay Nagargoje <dhananjay.nagargoje@sap.com>
2019-10-26 11:12:08 +0530

    contact list component added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import React, { Component } from 'react';
+import ListContacts from  './ListContacts';
+
+const contacts = [
+  {
+    "id": "dhananjay",
+    "name": "Dhananjay Nagargoje",
+    "email": "ng.dhananjay@gmail.com",
+    "avatarURL": "http://localhost:5001/dhananjay.jpg"
+  },
+  {
+    "id": "ryan",
+    "name": "Ryan Florence",
+    "email": "ryan@reacttraining.com",
+    "avatarURL": "http://localhost:5001/ryan.jpg"
+  },
+  {
+    "id": "michael",
+    "name": "Michael Jackson",
+    "email": "michael@reacttraining.com",
+    "avatarURL": "http://localhost:5001/michael.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "email": "tyler@reacttraining.com",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
+  }
+]
 
 class App extends Component {
   render() {
-    return "";
+    return (
+      <div>
+         <ListContacts contacts = {contacts} />  
+      </div>
+      )
   }
 }
 
