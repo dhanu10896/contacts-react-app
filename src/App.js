ef77a952e20c3745cadd8a1bd8f645c8f0af125f
Dhananjay Nagargoje <dhananjay.nagargoje@sap.com>
2019-10-27 01:34:38 +0530

    added create contact component with state based routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
 import * as ContactsAPI from './utils/ContactsAPI';
+import CreateContact from './CreateContact'
 
 class App extends Component {
 
   constructor(props) {
     super(props)
     this.state = {
+      screen: 'list', // list, create
       contacts: []
     }
 
@@ -29,7 +31,18 @@ class App extends Component {
   render() {
     return (
       <div>
-        <ListContacts onClickRemoveContact={this.removeContact} contacts={this.state.contacts} />
+        {this.state.screen === 'list' && (
+          <ListContacts
+          onClickRemoveContact={this.removeContact}
+            contacts={this.state.contacts}
+            onNavigate={() => {
+              this.setState({ screen: 'create' })
+            }}
+          />
+        )}
+        {this.state.screen === 'create' && (
+          <CreateContact/>
+        )}
       </div>
     )
   }
