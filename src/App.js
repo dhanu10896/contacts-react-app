2342c1830bfe71eb4324cf3bfa9110af82c1fef1
Dhananjay Nagargoje <dhananjay.nagargoje@sap.com>
2019-10-26 09:46:39 +0530

    starter code removed

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-class ContactList extends React.Component {
+class App extends Component {
   render() {
-    const people = this.props.contacts;
-    return <ol>
-      {people.map(person => (
-        <li key={person.name}>{person.name}</li>
-      ))}
-    </ol>
+    return "";
   }
-}
-
-function App() {
-  return (
-    <div className="App">
-      <ContactList contacts={[
-        {name : 'Dhananjay'},
-        {name : 'Lala'}
-      ]} />
-      <ContactList contacts={[
-        {name : 'Piyush'},
-        {name : 'Rishbh'}
-      ]} />
-    </div>
-  );
 }
 
 export default App;
